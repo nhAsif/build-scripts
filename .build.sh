d1def0f5ccacfa39c69f6327e0beaece250b41d1
Asif <49162687+nhAsif@users.noreply.github.com>
2022-01-22 10:39:14 +0600

    Update .build.sh

--- .build.sh
+++ .build.sh
@@ -24,14 +24,14 @@ git config --global user.email "najmulhasan3609@gmail.com" &&\
 git config --global user.name "nhAsif" &&\
 
 cd ~/android/lineage &&\
-repo init --depth=1 --no-repo-verify -u https://github.com/ArrowOS/android_manifest.git -b arrow-11.0 &&\
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b arrow .repo/local_manifests &&\
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 12.0 &&\
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cr-12 .repo/local_manifests &&\
 repo sync -j$(nproc --all) -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune &&\
 cd ~/android/lineage &&\
 source build/envsetup.sh &&\
-lunch arrow_rosy-userdebug &&\
+lunch lineage_rosy-userdebug &&\
 croot &&\
-m bacon | tee logs.txt
+mka bacon | tee logs.txt
 
 cd ~/android/lineage &&\
 curl --upload-file ./out/target/product/rosy/*.zip https://transfer.sh/nhalos.zip
