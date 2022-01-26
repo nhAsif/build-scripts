c3400f95f36dd5001f7a217749e5fced5700661a
Asif <49162687+nhAsif@users.noreply.github.com>
2022-01-26 19:05:50 +0600

    test f-los

--- .build.sh
+++ .build.sh
@@ -24,14 +24,14 @@ git config --global user.email "najmulhasan3609@gmail.com" &&\
 git config --global user.name "nhAsif" &&\
 
 cd ~/android/lineage &&\
-repo init --depth=1 --no-repo-verify -u git://github.com/ArrowOS/android_manifest.git -b arrow-12.0 -g default,-mips,-darwin,-notdefault &&\
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b arrow-12 .repo/local_manifests &&\
+repo init --depth=1 --no-repo-verify -u git://github.com/ForkLineageOS/android.git -b lineage-19.0 -g default,-mips,-darwin,-notdefault &&\
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b los19 .repo/local_manifests &&\
 repo sync -j$(nproc --all) -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune &&\
 cd ~/android/lineage &&\
 source build/envsetup.sh &&\
-lunch arrow_rosy-userdebug &&\
+lunch lineage_rosy-userdebug &&\
 croot &&\
-m bacon | tee logs.txt
+make bacon | tee logs.txt
 
 cd ~/android/lineage &&\
 curl --upload-file ./out/target/product/rosy/*.zip https://transfer.sh/nhalos.zip
