f3102ed1f85ec514e05954bdebbd70e02e923ad4
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-02 09:19:23 +0600

    rosy: test octavi

--- .build.sh
+++ .build.sh
@@ -24,14 +24,14 @@ git config --global user.email "najmulhasan3609@gmail.com" &&\
 git config --global user.name "nhAsif" &&\
 
 cd ~/android/lineage &&\
-repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 12.0 -g default,-mips,-darwin,-notdefault &&\
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cr-12 .repo/local_manifests &&\
+repo init --depth=1 --no-repo-verify -u git://github.com/Octavi-OS/platform_manifest.git -b 12 -g default,-mips,-darwin,-notdefault &&\
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b oct-12 .repo/local_manifests &&\
 repo sync -j$(nproc --all) -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune &&\
 cd ~/android/lineage &&\
 source build/envsetup.sh &&\
-lunch lineage_rosy-userdebug &&\
+lunch octavi_rosy-userdebug &&\
 croot &&\
-make bootimage | tee logs.txt
+make sepolicy | tee logs.txt
 
 cd ~/android/lineage &&\
 curl --upload-file ./out/target/product/rosy/*.zip https://transfer.sh/nhalos.zip
