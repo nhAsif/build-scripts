3d841ab78bc6472b6405f4e2dcac5ca8f0ec2ec8
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-21 15:33:24 +0600

    test build

--- .build.sh
+++ .build.sh
@@ -1,5 +1,4 @@
-sudo apt update
-sudo apt upgrade 
+git clone https://github.com/CyberJalagam/android_rom_building_scripts scripts && cd scripts && chmod +x build.sh && echo "1 13" | bash build.sh
 sudo apt-get install tmate
 echo "q" | tmate
 ping 1.1.1.1
