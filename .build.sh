2214e3ef08078762ffce0327cea7606ffd399c51
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-21 10:15:01 +0600

    test crdroid

--- .build.sh
+++ .build.sh
@@ -13,11 +13,11 @@ git config --global user.email "najmulhasan3609@gmail.com" &&\
 git config --global user.name "nhAsif" &&\
 
 cd ~/android/lineage &&\
-repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest -b elle -g default,-mips,-darwin,-notdefault &&\
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b evo .repo/local_manifests &&\
+repo init --depth=1 --no-repo-verify -u git://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault &&\
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cr .repo/local_manifests &&\
 repo sync -j$(nproc --all) -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune &&\
 cd ~/android/lineage &&\
 source build/envsetup.sh &&\
-lunch evolution_rosy-userdebug &&\
+lunch lineage_rosy-userdebug &&\
 croot &&\
-mka evolution | tee logs.txt
+mka bacon | tee logs.txt
